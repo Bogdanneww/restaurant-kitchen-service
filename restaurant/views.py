c885c011d9693523c2f7c010744b255bb56d5822
Bohdan <bogdannew@ukr.net>
2025-06-28 18:18:09 +0300

    Separate urls

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Welcome to restaurant</h1>")
