e7cb42fbe202186056065ec0ada5849c4a716144
Bohdan <bogdannew@ukr.net>
2025-06-28 15:45:05 +0300

    Update models

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,17 +7,16 @@ from restaurant_service import settings
 
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    country = models.CharField(max_length=255)
 
     class Meta:
         ordering = ["name"]
 
     def __str__(self):
-        return f"{self.name} {self.country}"
+        return f"{self.name}"
 
 
 class Cook(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    years_of_experience = models.IntegerField(default=0, null=True, blank=True)
 
     class Meta:
         verbose_name = "cook"
@@ -32,6 +31,8 @@ class Cook(AbstractUser):
 
 class Dish(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
 
