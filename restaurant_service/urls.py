8f16b9d5ca6ccf90723a9cb6d2901ba894e32044
Bohdan <bogdannew@ukr.net>
2025-06-28 11:31:33 +0300

    Start app: restaurant

--- restaurant_service/urls.py
+++ restaurant_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for restaurant_kitchen_service project.
+URL configuration for restaurant_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
