5140d76c6d2408f0d3c1a358e2a2662e88bf08a6
Bohdan <bogdannew@ukr.net>
2025-07-04 18:10:04 +0300

    add base.html

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from .models import Dish, Cook, DishType
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Welcome to restaurant</h1>")
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    context = {
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+    }
+    return render(request, "restaurant/index.html", context = context)
