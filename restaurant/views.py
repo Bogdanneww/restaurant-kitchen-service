b92c2d0535949a503c470c91205e7383f3afe1f1
Bohdan <bogdannew@ukr.net>
2025-07-09 19:53:39 +0300

    Add Home page visits counter

--- restaurant/views.py
+++ restaurant/views.py
@@ -9,10 +9,13 @@ def index(request: HttpRequest) -> HttpResponse:
     num_dishes = Dish.objects.count()
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
     context = {
         "num_dishes": num_dishes,
         "num_cooks": num_cooks,
         "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
     }
     return render(request, "restaurant/index.html", context = context)
 
