e3ab623841bf350b4dcd384e8ae751f8c85e5604
Bohdan <bogdannew@ukr.net>
2025-07-13 17:37:21 +0300

    Implement login and logout templates

--- restaurant_service/urls.py
+++ restaurant_service/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-path("restaurant/", include("restaurant.urls", namespace="restaurant")),
+    path("restaurant/", include("restaurant.urls", namespace="restaurant")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
