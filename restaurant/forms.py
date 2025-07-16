e6faf672b6d0d2a5be2d1ec4ef44b0a0a3c5b183
Bohdan <bogdannew@ukr.net>
2025-07-16 14:52:31 +0300

    Add Dish form for create and update views

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,9 +1,23 @@
+from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from restaurant.models import Cook
+from restaurant.models import Cook, Dish
 
 
 class CookCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Cook
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name")
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
